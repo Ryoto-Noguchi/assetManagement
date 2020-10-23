7082088fca33ff440d8c63176fc33893796dcbc4
Ryoto Noguchi <rnoguchi@ittomorrow.co.jp>
2020-10-23 18:19:52 +0900

    初回コミット確認

--- src/main/java/com/example/test/service/AssetService.java
+++ src/main/java/com/example/test/service/AssetService.java
@@ -11,5 +11,9 @@ public class AssetService {
     @Autowired
     AssetRepository assetRespository;
 
-    /* 資産IDを条件に */
+    /**資産IDを条件に資産詳細を取得する
+     * @param assetId 資産ID
+     * @return
+     */ 
+
 }
